44d7e0e60ed8c6414ea5fb269c39b51ea9fd0b9c
Jan Zalewski <18668118+jasiozet@users.noreply.github.com>
2022-07-24 20:12:58 +0200

    Switch to endpoint routing

--- api/Program.fs
+++ api/Program.fs
@@ -3,8 +3,9 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
-open Logic
 open Giraffe.ViewEngine
+open Giraffe.EndpointRouting
+open Logic
 
 let indexView =
   html [] [
@@ -19,17 +20,20 @@ let defaultPage = htmlView indexView
 let getHttpFromOperation operation (a,b) =
   ExecuteBasicOperation operation a b |> text
 
-let webApp =
-  choose [
+let endpoints =
+  [
     routef "/mul/%i/%i" (getHttpFromOperation Multiplication)
     routef "/add/%i/%i" (getHttpFromOperation Addition)
-    route "/json"   >=> json {| Message="Hello World"; Language="F#" |}
-    route "/"       >=> defaultPage
+    route "/json"       (json {| Message="Hello World"; Language="F#" |})
+    route "/"           defaultPage
   ]
 
 let configureApp (app : IApplicationBuilder) =
-    // Add Giraffe to the ASP.NET Core pipeline
-    app.UseGiraffe webApp
+    // Add Giraffe Endpoints to ASP.NET core
+    app
+      .UseRouting()
+      .UseEndpoints(fun e -> e.MapGiraffeEndpoints(endpoints))
+    |> ignore
 
 let configureServices (services : IServiceCollection) =
     // Add Giraffe dependencies
@@ -52,4 +56,4 @@ let main _ =
 //http://localhost:5000/add/2/3 => 5
 //http://localhost:5000/mul/2/3 => 6
 //http://localhost:5000/json => {"language":"F#","message":"Hello World"}
-//http://localhost:5000/anyRoute => htmled I |> F# Hello World
+//http://localhost:5000/ => htmled I |> F# Hello World
